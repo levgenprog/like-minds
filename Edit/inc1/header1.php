6d023a80d674be620c551b2413c2da7c95c0e614
Joseph <luckyman.rbk@gmail.com>
2020-05-07 17:53:25 +0600

    Include - Edit - folder

---
+++ Edit/inc1/header1.php
@@ -0,0 +1,16 @@
+<header id="header" class="alt">
+						<h1><a href="index.html">Diploma</a></h1>
+						<nav id="nav">
+							<ul>
+								<li class="special">
+									<a href="#menu" class="menuToggle"><span>Меню</span></a>
+									<div id="menu">
+										<ul>
+											<li><a href="#">Зарегистрироваться</a></li>
+											<li><a href="#">Авторизоваться</a></li>
+										</ul>
+									</div>
+								</li>
+							</ul>
+						</nav>
+					</header>
